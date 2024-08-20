a6a5a0d166521df7b8824d03b1ad4360b1fc61a8
Чиннов Владислав Андреевич <v.chinnov@pravo.tech>
2024-08-20 09:12:00 +0400

    Пример валидации

--- MemeGame/MemeGame.Domain/Games/Dto/GameCreatedDto.cs
+++ MemeGame/MemeGame.Domain/Games/Dto/GameCreatedDto.cs
@@ -1,11 +1,24 @@
-﻿namespace MemeGame.Domain.Games.Dto
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MemeGame.Domain.Games.Dto
 {
-    public class GameCreatedDto
+    public class GameCreatedDto : IValidatableObject
     {
+        public const int MinNameLength = 5;
 
         public string Name { get; set; }
 
-
         public GameSettingsDto GameSettings { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrWhiteSpace(Name) || Name.Length < MinNameLength)
+            {
+                throw new ValidationException(nameof(Name));
+            }
+
+            yield return ValidationResult.Success;
+        }
     }
 }
