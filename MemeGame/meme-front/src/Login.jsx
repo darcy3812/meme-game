85606ec9aa6dd66ec962199a79bb3e428a609788
Viktor <zeninoid@list.ru>
2024-09-06 17:45:45 +0400

    Переделал КАН-3 на Tailwind.Изменил цвета.Убрал градиент на логотипе.

--- MemeGame/meme-front/src/Login.jsx
+++ MemeGame/meme-front/src/Login.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./Login.css";
 import authApi from "./api/auth";
 
 const Login = (props) => {
@@ -11,18 +10,23 @@ const Login = (props) => {
         }
     };
     return (
-        <div className="login-container">
-            <div className="logo-box">Meme Game</div>
-            <div className="login-box">
+        <div className="mx-auto my-0 max-w-5xl box-border flex flex-col gap-y-12 justify-center items-center">
+            <div className="flex p-5 font-boom font-black text-big text-center justify-center bg-cyan-400 bg-clip-text text-transparent">
+                Meme Game
+            </div>
+            <div className="flex flex-col w-main min-w-52 max-w-5xl p-5">
                 <input
                     placeholder="Enter your nickname"
                     type="text"
-                    className="login-input"
+                    className="box-border w-full h-14 mt-0 mx-0 mb-2.5 py-0 pr-0 pl-4 bg-white text-black text-base focus:border-2 focus:border-solid focus:border-red-400 focus:shadow-normal focus:outline-offset-0 focus:outline-none"
                     value={login}
                     onChange={(e) => setLogin(e.target.value)}
                 />
 
-                <button className="login-button" onClick={() => postLogin()}>
+                <button
+                    className="bg-cyan-400 hover:bg-cyan-600 rounded-main border-none border-black border-2 px-2.5 py-1.5 text-darker text-xl font-normal text-wider leading-none text-center self-end h-14 w-full"
+                    onClick={() => postLogin()}
+                >
                     Login
                 </button>
             </div>
--- MemeGame/meme-front/tailwind.config.js
+++ MemeGame/meme-front/tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                boom: ["Boom", "sans-serif"],
+            },
+            fontSize: {
+                big: "calc(50px + 3.5vw)",
+            },
+            width: {
+                main: "calc(200px + 15vw)",
+            },
+            colors: {
+                pinks: "rgb(229, 126, 250)",
+                pinker: "rgb(224, 113, 246)",
+                violets: "rgb(101, 1, 121)",
+                violeter: "rgb(54, 0, 65)",
+                darker: "rgb(22, 6, 45)",
+            },
+            borderRadius: {
+                main: "77px",
+            },
+            boxShadow: {
+                normal: "0 0 3px #1670BE",
+            },
+        },
     },
     plugins: [],
 };
