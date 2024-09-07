4a4a42ac20ee7822027fe5c9590dbe480fd458b2
Viktor <zeninoid@list.ru>
2024-09-07 20:24:29 +0400

    Добавили валидацию на никнейм через useForm.

--- MemeGame/meme-front/src/Login.jsx
+++ MemeGame/meme-front/src/Login.jsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import authApi from "./api/auth";
 
 const Login = (props) => {
-    const [login, setLogin] = useState("");
-    const postLogin = async () => {
-        const res = await authApi.login(login);
+    const postLogin = async (form) => {
+        console.log(form);
+        const res = await authApi.login(form.login);
         if (res.status === 200) {
             props.setIsLogged(true);
         }
     };
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
     return (
         <div className="mx-auto my-0 max-w-5xl box-border flex flex-col gap-y-12 justify-center items-center">
             <div className="flex p-5 font-boom font-black text-big text-center justify-center bg-cyan-400 bg-clip-text text-transparent cursor-default">
@@ -18,14 +23,18 @@ const Login = (props) => {
                 <input
                     placeholder="Enter your nickname"
                     type="text"
-                    className="box-border rounded-main w-full h-14 mt-0 mx-0 mb-2.5 py-0 pr-0 pl-4 bg-white text-black text-base focus:border-2 focus:border-solid focus:border-red-400 focus:shadow-normal focus:outline-offset-0 focus:outline-none"
-                    value={login}
-                    onChange={(e) => setLogin(e.target.value)}
+                    className={`box-border rounded-main w-full h-14 mt-0 mx-0 mb-2.5 py-0 pr-0 pl-4 bg-white text-black text-base focus:outline-none ${
+                        errors.login ? "border-2 border-red-400" : ""
+                    }`}
+                    {...register("login", {
+                        required: true,
+                        validate: (value) => !!value.trim(),
+                    })}
                 />
 
                 <button
                     className="bg-cyan-400 hover:bg-cyan-600 rounded-main border-none border-black border-2 px-2.5 py-1.5 text-darker text-xl font-normal text-wider leading-none text-center self-end h-14 w-full active:scale-99"
-                    onClick={() => postLogin()}
+                    onClick={handleSubmit(postLogin)}
                 >
                     Login
                 </button>
